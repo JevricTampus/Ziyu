fbab6af472a104bfb18a3a88d7aabf8c90298e33
JevricTampus <jevric.tampus04@gmail.com>
2025-07-30 02:08:58 +0000

    ACT 3

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Jevric Tampus");
+Console.WriteLine("20241126307");
+Console.WriteLine("WHOS CAN RESIST WHEN YOUR FIRST LOVE COMES");
